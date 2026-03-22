159619da23d68a41a763165c8d9c30efdf118914
Waleed <waleedalfar1024@gmail.com>
2026-03-21 19:05:20 -0700

    no way

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://whisker-watch-zeta.vercel.app/",
+        "https://whisker-watch-zeta.vercel.app",
         "http://localhost:5173"
     ],
     allow_methods=["*"],
